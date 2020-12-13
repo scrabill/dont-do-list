4c6d362783775e5ea50075a9e49a0b6232d26bf5
scrabill <shannon.crabill@gmail.com>
2020-12-12 23:14:18 -0500

    Adds postgres database for list items

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres@localhost:5432/dontdoapp'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[
-        {'description': "Don't commit murder"},
-        {'description': "Don't leave the fridge door open long"},
-        {'description': "Don't wear white after Labor day"}
-    ])
+    return render_template('index.html', data=Todo.query.all())
